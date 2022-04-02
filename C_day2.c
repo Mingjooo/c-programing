d64da5b6c110ed48c57835e5eb3ae42e9a350d63
unknown <jmj030914@gmail.com>
2022-04-02 23:49:09 +0900

    쉽게 풀어쓴 c언어 3장

---
+++ C_day2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/*
+// 연봉 계산
+int main(void)
+{
+	int salary;
+	int deposit;
+
+	printf("월급을 입력하세요: ");
+	scanf_s("%d", &salary);
+
+	deposit = 10 * 12 * salary;
+
+	printf("10년 동안의 저축액: %d", deposit);
+
+	return 0;
+}
+*/
+/*
+// 원의 면적
+int main(void)
+{
+	float radius; // 반지름
+	float area;   // 면적
+	
+	printf("반지름을 입력하시오: ");
+	scanf_s("%f", &radius);
+
+	area = radius * radius * 3.14;
+
+	printf("원의 면적: %f", area);
+
+	return 0;
+}
+*/
+/*
+// 환율 계산
+int main(void)
+{
+	float rate; // 원/달러 환율
+	float usd;  // 달러화
+	int krw;    // 원화
+
+	printf("달러에 대한 원화 환율을 입력하시오: ");
+	scanf_s("%f", &rate);
+
+	printf("원화 금액을 입력하시오: ");
+	scanf_s("%d", &krw);
+
+	usd = krw / rate;
+
+	printf("원화 %d원은 %f달러입니다.", krw, usd);
+
+	return 0;
+}
+*/
