cd02684239718ed7ee4dd2601f2fdf2fc1b17cf9
unknown <jmj030914@gmail.com>
2022-04-11 17:18:58 +0900

    dito_2

---
+++ dito_2.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+/*
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 1;
+
+	scanf_s("%d", &num1);
+
+	while (num2 < 10)
+	{
+		printf("%d * %d = %d\n", num1, num2, num1 * num2);
+		num2++;
+	}
+
+	return 0;
+
+}
+*/
+/*
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+
+	while (num1 < 10)
+	{
+		while (num2 < 10)
+		{
+			printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			num2++;
+		}
+		num1++;
+		num2 = 1;
+		printf("\n");
+	}
+}
+*/
+/*
+int main(void)
+{
+	int num = 0;
+	
+	do
+	{
+		printf("%d\n", num);
+		num++;
+	} while (num < 3);
+
+	return 0;
+}
+*/
+/*
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 1;
+
+	while (num1 < 10)
+	{
+		while (num2 < 10)
+		{
+			printf("%d * %d = %d\n", num1, num2, num1 * num2);
+			num2++;
+		}
+		num1++;
+	}
+
+	return 0;
+}
+*/
+/*
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int c = 1;
+	while (a == 0)
+	{
+		printf("%d * %d = %d", b, c, b * c);
+		c++;
+		if (c == 10)
+		{
+			if (b == 9)
+			{
+				break;
+			}
+
+			b++;
+			c = 1;
+			printf("\n");
+		}
+
+	}
+}
+*/
+/*
+int main()
+{
+	for (int i = 9; i > 0; i--)
+	{
+		for (int k = 1; k < 10; k++)
+		{
+			printf("%d * %d = %d\t", k, i, k * i);
+		}
+		printf("\n");
+	}
+}
+*/
