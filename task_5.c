7ef7db08047c73011dc8bfa115ce11f42ca48ba5
unknown <jmj030914@gmail.com>
2022-04-18 20:50:35 +0900

    C프로그래밍 과제5

---
+++ task_5.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+/* 문제 1
+int main()
+{
+    int a = 0;
+    char str[] = "minju\n";
+    char ch;
+
+    while (str[a])
+    {
+        ch = str[a];
+        putchar(toupper(ch));
+        a++;
+    }
+    return 0;
+}
+*/
+/* 문제 2
+int main()
+{
+    int a = 0;
+    char str[10];
+    printf("");
+    scanf("%s", str);
+    char ch;
+
+    while (str[a])
+    {
+        ch = str[a];
+        putchar(tolower(ch));
+        a++;
+    }
+    return 0;
+}
+*/
